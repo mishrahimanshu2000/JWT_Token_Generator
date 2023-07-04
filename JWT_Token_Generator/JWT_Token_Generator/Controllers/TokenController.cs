1090f81f742f6034d8b6e8aec202a9bd9c8c4a6c
Himanshu Mishra <mishrahimanshu2000@gmail.com>
2023-07-04 13:53:27 +0530

    Initial addition of file

---
+++ JWT_Token_Generator/JWT_Token_Generator/Controllers/TokenController.cs
@@ -0,0 +1,42 @@
+﻿using JWT_Token_Generator.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JWT_Token_Generator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult<string> GenerateToken([FromBody] Token token)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(token.SecretKey));
+            var credintials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, token.Username),
+                new Claim(ClaimTypes.Role, token.Roles[0]),
+                new Claim("tenantId", "T5")
+            };
+            foreach (var item in token.Roles)
+            {
+                claims.Append(new Claim(ClaimTypes.Role, item));
+                
+            }
+            var jwtToken = new JwtSecurityToken(
+                    issuer: token.Issuer.ToString(),
+                    audience: token.Audience.ToString(),
+                    expires: token.ExpirationTime,
+                    claims: claims,
+                    signingCredentials : credintials
+                );
+            
+            return Ok(new JwtSecurityTokenHandler().WriteToken(jwtToken));
+        } 
+    }
+}
---
+++ JWT_Token_Generator/JWT_Token_Generator/Model/Token.cs
@@ -0,0 +1,13 @@
+﻿namespace JWT_Token_Generator.Model
+{
+    public class Token
+    {
+        public string Issuer { get; set; } = null!;
+        public string Audience { get; set; } = null!;
+        public DateTime IssuedAt { get; set; } = DateTime.Now;
+        public DateTime ExpirationTime { get; set; } = DateTime.Now;
+        public string Username { get; set; } = null!;
+        public List<string> Roles { get; set; } = new List<string>();
+        public string SecretKey { get; set; } = null!;
+    }
+}
